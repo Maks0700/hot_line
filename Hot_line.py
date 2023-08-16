60237edc2360f66b8b30fb8731fc162759e85be9
unknown <rukhlya1999@mail.ru>
2023-08-16 15:40:03 +0300

    New for hl

--- Hot_line.py
+++ Hot_line.py
@@ -1,67 +1,74 @@
-#The topic----the modules of Python 
 
 
-
-exc_name=["Василий","Игорь","Андрей","Сергей","Матвей","Евгений","Дмитрий","Юрий","Алексей"]
-while True:
-    name=input("Введите ваше имя: ")
-    father=input("Введите имя отца: ")
- #мужики   
+def define(name,father):
     
-    if name.lower()=="никита":
-        
-        if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
-                print(name+" "+father.replace("ий","ьевич"))
-        elif father=="илья":
-                 print(name+" "+father.replace("ья","ич"))
-        else:
-                print(name+" "+father.replace("й","евич"))
-    else:
-                print(f"{name} {father}ович")        
-        
-    elif name.lower()=="илья":
-        if father.endswith("й"):
-            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
-                print(name+" "+father.replace("ий","ьевич"))
-            elif father=="илья":
-                 print(name+" "+father.replace("ья","ич"))
-            else:
-                print(name+" "+father.replace("й","евич"))
-        else:
-                print(f"{name} {father}ович")        
-            
-    
-    
-    
-    
-    
-    
-    elif name.endswith("а") or name.endswith("я"):
-#телки
-         if father.endswith("й"):
-            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений"or father.lower()=="анатолий":
-                print(name+" "+father.replace("ий","ьевна"))
-            else:
-                print(name+" "+father.replace("й","евна"))
-         elif father=="илья":
-                print(name+" "+father.replace("я","инична"))
-         
-         else:
-                print(name+" "+father.replace("а","овна"))        
                     
-    else:
-#мужики без никиты и ильи        
-        if father.endswith("й"):
-            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
-                print(name+" "+father.replace("ий","ьевич"))
-            
-            elif father=="илья":
-                 print(name+" "+father.replace("ья","ич"))
-            else:
-                print(name+" "+father.replace("й","евич"))
-        else:
-                print(f"{name} {father}ович")        
-           
+                    total=""
+                                  
+                
+                    if name.lower()=="никита":
+                        
+                        if father.endswith("й"):
+                            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
+                                total=name+" "+father.replace("ий","ьевич")
+                            else:
+                               total=(name+" "+father.replace("й","евич"))
+                        elif father=="илья":
+                                total=(name+" "+father.replace("ья","ьич"))
+                        elif father=="игорь":
+                            total=(name+" "+father+"евич")
+                        
+                        else:
+                                total=(f"{name} {father}ович")        
+                        
+                    elif name.lower()=="илья":
+                        if father.endswith("й"):
+                            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
+                                total=(name+" "+father.replace("ий","ьевич"))
+                            else:
+                                total=(name+" "+father.replace("й","евич"))
+                        elif father=="илья":
+                                total=(name+" "+father.replace("ья","ьич"))
+                        elif father=="игорь":
+                            total=(name+" "+father+"евич")
+                        
+                        else:
+                                total=(f"{name} {father}ович")        
+                            
+                    elif name.endswith("а") or name.endswith("я"):
+               
+                        if father.endswith("й"):
+                            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений"or father.lower()=="анатолий":
+                                total=(name+" "+father.replace("ий","ьевна"))
+                            else:
+                                total=(name+" "+father.replace("й","евна"))
+                        elif father=="илья":
+                                total= (name+" "+father.replace("я","инична"))
+                        elif father=="игорь":
+                            total=(name+" "+father+"евна")
+                        
+                        else:
+                                total=(name+" "+father+"овна")        
+                                    
+                    else:
+                       
+                        if father.endswith("й"):
+                            if father.lower()=="юрий" or father.lower()=="василий" or father.lower()=="евгений" or father.lower()=="анатолий":
+                                total=(name+" "+father.replace("ий","ьевич"))
+                            else:
+                                total=(name+" "+father.replace("й","евич"))
+                        elif father=="илья":
+                                total=(name+" "+father.replace("ья","ьич"))
+                        elif father=="игорь":
+                            total=(name+" "+father+"евич")
+                        else:
+                                total=(f"{name} {father}ович")        
+                    return total
+
+
+
+
+
 
 
 
